19400fb01f17b4f35ab0f0fa4e3fcee18716226c
sidi mohamed <sidimoh.mouta@gmail.com>
2025-03-08 21:49:07 +0100

    df

--- config/db.js
+++ config/db.js
@@ -1,1 +1,2 @@
 // Connexion à la base de données
+// Basde de donnes premier commit test
--- src/app.js
+++
@@ -1,3 +0,0 @@
-const express = require('express');
-const app = express();
-app.listen(3000, () => console.log('Serveur en marche sur http://localhost:3000'));
